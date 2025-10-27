92937575dab3fba0e7ee7b09a5cfbec0c99f8348
Abdullah-537 <243537@students.au.edu.pk>
2025-10-27 06:54:19 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ASSIGNEMENT_2;
+using System;
+using System.Windows.Forms;
+
+
+namespace assignemnt_2_vp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
